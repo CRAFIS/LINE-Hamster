0bf59562a9818a0f43067d2396c252cbdf40149c
CRAFIS <crafisbandicoot@gmail.com>
2019-07-01 11:48:50 +0900

    Update

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, abort
-
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, VideoSendMessage
-import os, requests, json
+from requests_oauthlib import OAuth1Session
+import os, requests, json, random
 
 app = Flask(__name__)
 
-#環境変数取得
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 COTOGOTO_APPKEY = os.environ["COTOGOTO_APPKEY"]
+TWITTER_CONSUMER_KEY = os.environ["TWITTER_CONSUMER_KEY"]
+TWITTER_CONSUMER_SECRET = os.environ["TWITTER_CONSUMER_SECRET"]
+TWITTER_ACCESS_TOKEN = os.environ["TWITTER_ACCESS_TOKEN"]
+TWITTER_ACCESS_SECRET = os.environ["TWITTER_ACCESS_SECRET"]
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
@@ -19,25 +22,22 @@ handler = WebhookHandler(CHANNEL_SECRET)
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
     if '動画' in text or 'ビデオ' in text:
-        video_url = "https://video.twimg.com/ext_tw_video/1145316011974877190/pu/vid/1280x720/RdiweV650PpxBqH8.mp4"
-        preview_url = "https://pbs.twimg.com/ext_tw_video_thumb/1145316011974877190/pu/img/iL6-Bzc-Z4EygSlZ.jpg"
+        query = "#ハムスターのいる生活 filter:videos"
+        video_url, preview_url = get_video(query)
         line_bot_api.reply_message(
             event.reply_token,
             VideoSendMessage(original_content_url=video_url, preview_image_url=preview_url))
@@ -61,6 +61,24 @@ def get_reply(text):
         reply_text = "🐹💨🐹💨🐹💨"
     return reply_text
 
+def get_video(query):
+    twitter = OAuth1Session(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+    url = "https://api.twitter.com/1.1/search/tweets.json"
+    params = {
+        "q": query,
+        "lang": "ja",
+        "result_type": "mixed",
+        "count": 100
+    }
+    res = twitter.get(url, params = params)
+    tweets = json.loads(res.text)["statuses"]
+    tweet = random.choice(tweets)
+    tweet = tweet.get("retweeted_status", tweet)
+    variants = tweet["extended_entities"]["media"][0]["video_info"]["variants"]
+    variants = [variant for variant in variants if variant.get("bitrate")]
+    video_url = max(variants, key = lambda x: x["bitrate"])["url"]
+    preview_url = tweet["extended_entities"]["media"][0]["media_url"]
+    return video_url, preview_url
 
 if __name__ == "__main__":
 #    app.run()
